3af190d35b2eaeb62af3dbbecd581eacb9bb65cb
Nicholas Kelton <kelton.nicholas@gmail.com>
2019-01-28 05:59:40 -0600

    fix configuration for logger

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import config
 
 litter_bug = ['python3', config.BASE_PATH + 'main.py']
 
-logging.basicConfig(filename=config.LOG, format='%(asctime)s %(message)s', stream=sys.stdout)
+logging.basicConfig(filename=config.LOG, format='%(asctime)s %(message)s')
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
