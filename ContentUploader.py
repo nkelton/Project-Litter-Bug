9d58b661ecfbfe7f528858d6be88fbd3b4b6ca50
Nicholas Kelton <kelton.nicholas@gmail.com>
2019-01-31 16:57:33 -0600

    Add thumbnail upload functionality upload flow

--- ContentUploader.py
+++ ContentUploader.py
@@ -1,10 +1,17 @@
 import logging
 import os
 import subprocess
+import sys
 
+import httplib2
 import requests
+from apiclient.discovery import build
+from googleapiclient.errors import HttpError
 from moviepy.video.VideoClip import ImageClip, ColorClip, TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run_flow
 
 import config
 
@@ -25,31 +32,19 @@ class ContentUploader(object):
         self.weight = None
 
     def upload_content(self):
-        logger.warning('Creating thumbnail...')
-        self.create_thumbnail()
         logger.warning('Getting weight...')
         self.get_weight()
         logger.warning('Uploading...')
         self.upload()
         logger.warning('Storing...')
         self.store()
-
-    def create_thumbnail(self):
-        color = (255, 255, 255)
-        size = (1280, 720)
-        background = ColorClip(size, color)
-        logo = ImageClip(self.logo_path) \
-            .resize(width=400, height=200) \
-            .set_pos(('center', 'center'))
-        text = TextClip(txt=str(self.id), size=(500, 500)).set_position(
-            ('center', 'bottom'))
-        logger.warning('Saving thumbnail...')
-        CompositeVideoClip([background, logo, text]).save_frame(self.thumb_path)
-        logger.warning('Thumbnail saved...')
+        logger.warning('Creating thumbnail...')
+        self.create_thumbnail()
+        logger.warning('Setting thumbnail...')
+        self.set_thumbnail()
 
     def upload(self):
         logger.warning('attempting to upload file...')
-        process_output = []
         category = 'Science & Technology'
         description = self.create_description()
         logger.warning('title: ' + self.name[:-4])
@@ -59,19 +54,13 @@ class ContentUploader(object):
         logger.warning('thumb_path: ' + self.thumb_path)
         logger.warning('results_path: ' + self.result_path)
         logger.warning('secret_path: ' + self.secret_path)
-
-        if os.path.exists(self.thumb_path):
-            logger.warning('THUMB PATH EXISTS!!!')
-
-        if os.path.exists(self.result_path):
-            logging.warning('RESULTS PATH EXISTS!!!')
+        process_output = []
 
         upload_cmd = ['youtube-upload',
                       '--title=' + self.name[:-4],
                       '--description=' + description,
                       '--category=' + category,
                       '--tags=' + self.tags,
-                      '--thumbnail=' + self.thumb_path,
                       '--client-secrets=' + self.secret_path,
                       self.result_path]
         try:
@@ -83,8 +72,6 @@ class ContentUploader(object):
             proc.wait()
             self.url = self.extract_url(process_output)
             logger.warning('resulting url: ' + self.url)
-
-
 
         except subprocess.CalledProcessError as e:
             logger.error('Error called in ContentUploader.upload()')
@@ -158,6 +145,48 @@ class ContentUploader(object):
             'weight': self.weight,
         })
         logger.error('POST RESPONSE: ' + response.text)
+
+    def create_thumbnail(self):
+        color = (255, 255, 255)
+        size = (1280, 720)
+        background = ColorClip(size, color)
+        logo = ImageClip(self.logo_path) \
+            .resize(width=400, height=200) \
+            .set_pos(('center', 'center'))
+        text = TextClip(txt=str(self.id), size=(500, 500)).set_position(
+            ('center', 'bottom'))
+        logger.warning('Saving thumbnail...')
+        CompositeVideoClip([background, logo, text]).save_frame(self.thumb_path)
+        logger.warning('Thumbnail saved...')
+
+    def set_thumbnail(self):
+        def get_authenticated_service():
+            flow = flow_from_clientsecrets(config.SECRET_PATH,
+                                           scope=config.YOUTUBE_READ_WRITE_SCOPE,
+                                           message=config.MISSING_CLIENT_SECRETS_MESSAGE)
+
+            storage = Storage("%s-oauth2.json" % sys.argv[0])
+            credentials = storage.get()
+
+            if credentials is None or credentials.invalid:
+                credentials = run_flow(flow, storage)
+
+            return build(config.YOUTUBE_API_SERVICE_NAME, config.YOUTUBE_API_VERSION,
+                         http=credentials.authorize(httplib2.Http()))
+
+        def upload_thumbnail(youtube, video_id):
+            youtube.thumbnails().set(
+                videoId=video_id,
+                media_body=config.THUMB_PATH
+            ).execute()
+
+        youtube = get_authenticated_service()
+        try:
+            upload_thumbnail(youtube, self.url[-11:])
+        except HttpError:
+            logger.error("Error occured setting thumbnail...")
+        else:
+            logger.warning("Thumbnail successfully set.")
 
     @staticmethod
     def _url(path):
